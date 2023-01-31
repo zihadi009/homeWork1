9eeab6bf03db1a036ef34f99e5be9bd4cfc1dc88
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2023-01-31 22:11:04 +0600

    first commit

---
+++ homeWorkDay1.java
@@ -0,0 +1,13 @@
+// --------------------------------
+/* Home Work 01 */
+// --------------------------------
+class Information {
+    public static void main(String[] args) {
+        System.out.println("Name: Md. Musfiqur Rahman");
+        System.out.println("ID: 221-15-4641");
+        System.out.println("Current Address: YKSG-2");
+        System.out.println("Department: Computer Science and Engineering");
+        System.out.println("Favourite Game: Football");
+        System.out.println("Hobby: Singing a song, Coding");
+    }
+}
